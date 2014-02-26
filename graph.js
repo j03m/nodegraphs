e433fef637dde8c429d32493fa15984712954548
Joe Mordetsky <jmordetsky@zynga.com>
2014-02-26 11:41:05 -0500

    Starting the graph library

---
+++ graph.js
@@ -0,0 +1,134 @@
+var AdjencyList = function(config){
+    this.edgeData = config.edgeData;
+    this.directed = config.directed;
+    this.init();
+}
+
+AdjencyList.prototype.init = function(){
+    this.edges = [];
+    this.degree = [];
+    this.numEdges = 0;
+    this.numVertices = 0;
+    this.initEdges();
+}
+
+AdjencyList.prototype.initEdges = function(){
+    if (this.edgeData){
+        this.createFromEdgeData();
+    }else{
+        this.createRandomEdges();
+    }
+}
+
+AdjencyList.prototype.createFromEdgeData = function(){
+    for (var i =0;i<this.edgeData.length; i++){
+        var edge = this.edgeData[i];
+        this.insertEdge(edge.from, edge.to, this.directed);
+    }
+}
+
+AdjencyList.prototype.insertEdge = function (from, to, directed){
+    if (parseInt(from)==NaN || from > this.edgeData.length){
+        throw "invalid edge from: " + from;
+    }
+
+    if (parseInt(to)==NaN || to > this.edgeData.length){
+        throw "invalid edge to: " + to;
+    }
+
+
+    this.numEdges++;
+    this.numVertices++;
+
+    //build the adjency list by inserting at head
+    var edgeNode = {};               //0,1 comes in 0->1
+    //the next node in my adjency list points TO
+    edgeNode.number = to; //this node goes from 1
+
+    var head = this.edges[from];
+    edgeNode.next = head;
+
+    //insert at head
+    this.edges[from] = edgeNode;
+
+    if (!directed){
+        //flip the parameters around - to is now from, from is now to, indicate that directed is true temp to avoid infinite recurse
+        this.insertEdge(to, from, true);
+    }
+
+}
+
+AdjencyList.prototype.createRandomEdges = function(){
+    throw "not implemented."
+}
+
+AdjencyList.prototype.bfs = function(start){
+    //init tracking
+    this.initBfs();
+    this.queue.push(start);
+    this.discovered[start]=true;
+    while (this.queue.length > 0){
+        //pull a node from the queue
+        var node = this.queue.shift();
+        //process it
+        this.processVertexEarly(node);
+        //mark it as processed
+        this.processed[node] = true;
+
+        //get the adjacency list for this node (which equates to getting the first child/head
+        var list = this.edges[node];
+        while(list){
+            //get the number of this child
+            var childNode = list.number;
+            //if it wasn't processed or if this is a directed graph (I don't get this quite yet)
+            if (!this.processed[childNode] || this.directed){
+                this.processEdge(node, childNode);
+            }
+
+            //if it hasn't been discovered
+            if (!this.discovered[childNode]){
+                //queue it up
+                this.queue.push(childNode);
+                //set it as discovered
+                this.discovered[childNode] = true;
+
+                //indicate that the parent of the childnode is node
+                this.parent[childNode] = node;
+            }
+
+            list = list.next; //traverse the list
+        }
+
+        this.processVertexLate(node);
+
+    }
+}
+
+AdjencyList.prototype.processVertexEarly = function(){
+
+}
+
+AdjencyList.prototype.processVertexLate = function(){
+
+}
+
+AdjencyList.prototype.processEdge = function(){
+
+}
+
+
+AdjencyList.prototype.initBfs = function(){
+    this.processed = [];
+    this.discovered = [];
+    this.parent = [];
+    this.queue = [];
+}
+
+module.exports.AdjencyList = AdjencyList;
+
+
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,18 @@
+var AdjencyList = require('./graph.js').AdjencyList;
+
+var config = {
+    "directed":false,
+    "edgeData":[
+        {"from":0, "to":1},
+        {"from":1, "to":2},
+        {"from":2, "to":3},
+        {"from":3, "to":4},
+        {"from":4, "to":5},
+        {"from":5, "to":6},
+        {"from":6, "to":7},
+        {"from":7, "to":8},
+        {"from":8, "to":9}
+    ]
+}
+
+var al = new AdjencyList(config);
