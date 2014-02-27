550b601d2cc3180ef2f1c54596e9eeb069c7eba7
Joe Mordetsky <jmordetsky@zynga.com>
2014-02-27 13:37:04 -0500

    Bug fixes and final on connections.

--- connections.js
+++ connections.js
@@ -16,10 +16,21 @@ var configComplex = {
         {from:5, to:4},
         {from:4, to:3},
     ]
-
-
 }
 
-var al = new ConnectedAdjacencyList(configComplex);
+var configDisconnected = {
+    "directed":false,
+    "edgeData":[
+        {from:0, to:1},
+        {from:1, to:2},
+        {from:3, to:4},
+        {from:4, to:5},
+        {from:6, to:7},
+        {from:7, to:8},
+        {from:8, to:9}
+    ]
+}
+
+var al = new ConnectedAdjacencyList(configDisconnected);
 al.bfs(0);
 al.findConnectedComponents();
