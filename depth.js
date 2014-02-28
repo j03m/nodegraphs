81b071a1f3d152ad91a9a749085f2d9d46ae4fa1
Joe Mordetsky <jmordetsky@zynga.com>
2014-02-27 21:36:15 -0500

    Changed config data in depth for a better illustration.

--- depth.js
+++ depth.js
@@ -5,44 +5,15 @@ var config = {
     "edgeData":[
         {"from":0, "to":1},
         {"from":1, "to":2},
-        {"from":2, "to":3},
+        {"from":2, "to":4},
+        {"from":0, "to":3},
         {"from":3, "to":4},
         {"from":4, "to":5},
-        {"from":5, "to":6},
-        {"from":6, "to":7},
-        {"from":7, "to":8},
-        {"from":8, "to":9}
+        {"from":3, "to":6}
+
+
     ]
 }
 
-var configDirected = {
-    "directed":true,   //note this should be directed, because our raw data will repeat relationships
-    "edgeData":[]
-}
-
-for(var i =0;i<10;i++){
-    for (var ii=0;ii<10;ii++){
-        if (i != ii){
-            configDirected.edgeData.push({"from":i, "to":ii});
-        }
-    }
-}
-
-var configComplex = {
-    "directed":false,
-    "edgeData":[
-        {from:0, to:2},
-        {from:2, to:1},
-        {from:1, to:4},
-        {from:0, to:5},
-        {from:2, to:5},
-        {from:5, to:0},
-        {from:2, to:3},
-        {from:3, to:5},
-        {from:5, to:4},
-        {from:4, to:3},
-    ]
-}
-
-var al = new AdjacencyList(configDirected);
-al.dfs(0);
+var al = new AdjacencyList(config);
+al.bfs(0);
